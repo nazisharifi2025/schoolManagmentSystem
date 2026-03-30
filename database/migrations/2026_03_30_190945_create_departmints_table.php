9e76d91ad355e5c8f2dc0eade32edf9ef133de83
nazi sharifi <banonazisharifi@gmail.com>
2026-03-30 23:41:56 +0430

    add table department

--- database/migrations/2026_03_30_190945_create_departmints_table.php
+++ database/migrations/2026_03_30_190945_create_departmints_table.php
@@ -11,10 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('schools', function (Blueprint $table) {
+        Schema::create('departmints', function (Blueprint $table) {
             $table->id();
             $table->string("name");
-            $table->string("whatsapp-link");
+            $table->foreignId("school_id")->constrained("schools")->cascadeOnDelete();
             $table->timestamps();
         });
     }
@@ -24,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('schools');
+        Schema::dropIfExists('departmints');
     }
 };
