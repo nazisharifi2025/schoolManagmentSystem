179c8da399f6005359406c819a514766c2ccd69c
nazi sharifi <banonazisharifi@gmail.com>
2026-04-01 23:42:16 +0430

    add schooluser tble

--- database/migrations/2026_04_01_190119_create-school_user.php
+++ database/migrations/2026_04_01_190119_create-school_user.php
@@ -12,6 +12,7 @@ return new class extends Migration
     public function up(): void
     {
          Schema::table('schoolUsers', function (Blueprint $table) {
+            $table->id();
             $table->foreignId("school_id")->constrained("schools")->onDelete("cascade");
             $table->foreignId("user_id")->constrained("users")->onDelete("cascade");
             $table->timestamps();
