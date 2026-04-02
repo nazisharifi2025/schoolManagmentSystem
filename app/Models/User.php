974aa593c73ae8e881ca15285a7b63070489ced4
nazi sharifi <banonazisharifi@gmail.com>
2026-04-02 10:06:23 +0430

    add relations

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,6 +5,7 @@ namespace App\Models;
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
@@ -23,6 +24,9 @@ class User extends Authenticatable
         'email',
         'password',
     ];
+    public function school(){
+        return $this->belongsTo(School::class , "schools");
+    }
 
     /**
      * The attributes that should be hidden for serialization.
