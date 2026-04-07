4ea2d5063a885af6bcd579de1ec7f4f6e868c664
nazi sharifi <banonazisharifi@gmail.com>
2026-04-07 18:24:18 +0430

    add component add cantract

---
+++ app/Livewire/Cantract/Add.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Cantract;
+
+use Livewire\Component;
+
+class Add extends Component
+{
+    public function render()
+    {
+        return view('livewire.cantract.add');
+    }
+}
