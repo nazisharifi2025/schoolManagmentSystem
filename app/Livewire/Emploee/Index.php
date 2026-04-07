c206f996852da37c17dd33e235f6f5f986a0dee9
nazi sharifi <banonazisharifi@gmail.com>
2026-04-07 18:34:39 +0430

    add component index emploee

---
+++ app/Livewire/Emploee/Index.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Emploee;
+
+use Livewire\Component;
+
+class Index extends Component
+{
+    public function render()
+    {
+        return view('livewire.emploee.index');
+    }
+}
