cac3c482aed1feac48893c6854381dd2e9460ac9
nazi sharifi <banonazisharifi@gmail.com>
2026-04-08 23:42:21 +0430

    add style

--- resources/views/livewire/payroll.blade.php
+++ resources/views/livewire/payroll.blade.php
@@ -1,3 +1,3 @@
-<div class=" h-64 w-full flex items-center gap-4 bg-gray-100 ">
+<div class=" h-64 w-full flex items-center gap-4 bg-gray-100  justify-center rounded-lg">
     {{-- Close your eyes. Count to one. That is how long forever feels. --}}
 </div>
