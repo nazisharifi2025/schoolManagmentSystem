fb86ae9e3a4422c446ac52ff32e1653f62a70e34
nazi sharifi <banonazisharifi@gmail.com>
2026-04-10 16:23:07 +0430

    add feater component

---
+++ resources/views/livewire/feature.blade.php
@@ -0,0 +1,4 @@
+<div class=" max-w-6xl mx-auto">
+    
+    {{-- Nothing in the world is as soft and yielding as water. --}}
+</div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,11 @@ export default {
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
+            colors:{
+                brand: "#09637E",
+                bg: "#F34ED",
+                primary: '#424642'
+            }
         },
     },
 
