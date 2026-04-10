fbfdf11f035f8e5deb1610e1ec40684851b4b137
nazi sharifi <banonazisharifi@gmail.com>
2026-04-10 16:56:54 +0430

    add feater component

--- resources/views/livewire/feature.blade.php
+++ resources/views/livewire/feature.blade.php
@@ -1,6 +1,7 @@
 <div class=" max-w-6xl mx-auto">
     <div class="flex flex-col gap-3 w-full mt-6 items-center">
         <h1 class=" text-xl md:text-4xl lg:text-6xl font-bold">Sharifi High School</h1>
+        <p>Ustad Sharifi & Bibi Zahra High School" is one of the leading educational institutions in the Houtqol region (Jaghori District, Ghazni Province), dedicated to enhancing knowledge and nurturing the younger generation. By employing experienced teaching staff and maintaining a disciplined environment, this school carries out its educational programs in accordance with the standards of the Ministry of Education.Key Features:</p>
     </div>
     {{-- Nothing in the world is as soft and yielding as water. --}}
 </div>
