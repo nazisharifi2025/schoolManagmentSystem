ba7f0bf9fedf45ea6f78cb6dd67562cf6028f9ac
nazi sharifi <banonazisharifi@gmail.com>
2026-04-10 16:31:32 +0430

    add feater component

--- resources/views/livewire/feature.blade.php
+++ resources/views/livewire/feature.blade.php
@@ -1,4 +1,4 @@
 <div class=" max-w-6xl mx-auto">
-    <div class="flex flex-col "></div>
+    <div class="flex flex-col gap-3 w-full"></div>
     {{-- Nothing in the world is as soft and yielding as water. --}}
 </div>
