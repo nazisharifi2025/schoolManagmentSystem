b9e08ca0f7e2a6b6fb5737bcd8779cee92ea5a7a
nazi sharifi <banonazisharifi@gmail.com>
2026-04-10 16:33:31 +0430

    add feater component

--- resources/views/livewire/feature.blade.php
+++ resources/views/livewire/feature.blade.php
@@ -1,6 +1,6 @@
 <div class=" max-w-6xl mx-auto">
     <div class="flex flex-col gap-3 w-full mt-6 items-center">
-        <h1>Sharifi High School</h1>
+        <h1 class=" text-xl md:text-4xl lg:text-6xl font-bold">Sharifi High School</h1>
     </div>
     {{-- Nothing in the world is as soft and yielding as water. --}}
 </div>
