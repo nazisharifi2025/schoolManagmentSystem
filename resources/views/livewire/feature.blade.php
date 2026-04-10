95b4488774116b11946ee794cd28cd9dc78408ff
nazi sharifi <banonazisharifi@gmail.com>
2026-04-10 22:44:04 +0430

    add new code

--- resources/views/livewire/feature.blade.php
+++ resources/views/livewire/feature.blade.php
@@ -4,12 +4,13 @@
         <p class=" text-center w-10/12">Ustad Sharifi & Bibi Zahra High School" is one of the leading educational institutions in the Houtqol region (Jaghori District, Ghazni Province), dedicated to enhancing knowledge and nurturing the younger generation. By employing experienced teaching staff and maintaining a disciplined environment, this school carries out its educational programs in accordance with the standards of the Ministry of Education.Key Features:</p>
     </div>
     <div class=" w-full grid grid-cols-3 gap-5">
-        <div class=" w-full col-span-2 bg-gray-50 shadow-[0px_0px_0px_rgba(0,0,0,0.4)] h-48">
+        <div class=" w-full p-4 flex items-center col-span-2 bg-gray-50 shadow-[0px_0px_0px_rgba(0,0,0,0.4)] h-fit">
             <div class="flex flex-col ga-2">
                 <h1 class="text-3xl font-bold ">Manager</h1>
                 <h1 class="text-4xl font-bold ">Esmatullah Gholami</h1>
-                <p class=" text-center w-10/12">Ustad Sharifi & Bibi Zahra High School" is one of the leading educational institutions in the Houtqol region (Jaghori District, Ghazni Province), dedicated to enhancing knowledge and nurturing the younger generation. By employing experienced teaching staff and maintaining a disciplined environment, this school carries out its educational programs in accordance with the standards of the Ministry of Education.Key Features:</p>
+                <p class=" w-10/12">Ustad Sharifi & Bibi Zahra High School" is one of the leading educational institutions in the Houtqol region (Jaghori District, Ghazni Province), dedicated to enhancing knowledge and nurturing the younger generation. By employing experienced teaching staff and maintaining a disciplined environment, this school carries out its educational programs in accordance with the standards of the Ministry of Education.Key Features:</p>
             </div>
+            <img src="images/image.jpg" alt="Manager" class="w-52 h-52 border-8 border-brand object-cover ml-4">
         </div>
         <div class=" w-full  bg-gray-50 shadow-[0px_0px_0px_rgba(0,0,0,0.8)] h-48"></div>
     </div>
