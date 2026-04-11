7c0918ebb8a0dcf3e3133fe0dbb2a6403127b0da
nazi sharifi <banonazisharifi@gmail.com>
2026-04-11 20:43:46 +0430

    add new comonent and logec

--- resources/views/livewire/faq.blade.php
+++ resources/views/livewire/faq.blade.php
@@ -1,9 +1,9 @@
 <div class=" max-w-6xl mx-auto my-5 flex flex-col gap-4">
     <h1 class="text-5xl font-bold text-brand">FAQ<h1>
         @foreach ($questions as $q)
-        <div class=" p-4 flex items-center border-b border-gray-300">
+        <div class=" p-4 flex flex-col justify-center border-b border-gray-300">
             <h1 class=" text-4xl font-bold">{{ $q[0] }}</h1>
-            <h1 class=" text-4xl font-bold">{{ $q[1] }}</h1>
+            <h1 class=" text-xl font-bold">{{ $q[1] }}</h1>
         </div>
         @endforeach
   
