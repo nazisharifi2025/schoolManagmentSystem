6ce2b4a8be7be1322b6bc1abee7eb1a15d90a116
nazi sharifi <banonazisharifi@gmail.com>
2026-04-11 20:40:55 +0430

    add new comonent and logec

--- resources/views/livewire/faq.blade.php
+++ resources/views/livewire/faq.blade.php
@@ -1,9 +1,10 @@
 <div class=" max-w-6xl mx-auto my-5 flex flex-col gap-4">
     <h1 class="text-5xl font-bold text-brand">FAQ<h1>
-        @for
-    <div class=" p-4 flex items-center border-b border-gray-300">
-        <h1 class=" text-4xl font-bold">is one of the leading educational institutions ?</h1>
-    </div>
+        @foreach ($questions as $q)
+        <div class=" p-4 flex items-center border-b border-gray-300">
+            <h1 class=" text-4xl font-bold">{{ $q[0] }}</h1>
+        </div>
+        @endforeach
   
     {{-- Nothing in the world is as soft and yielding as water. --}}
 </div>
