4e2e8820eb268e274d877a84b2d95b8460f018b8
nazi sharifi <banonazisharifi@gmail.com>
2026-04-22 00:02:53 +0430

    add new seeder file

--- database/seeders/SchoolSeeder.php
+++ database/seeders/SchoolSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class SchoolSeeder extends Seeder
 {
@@ -12,6 +13,9 @@ class SchoolSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+       DB::table("schools")->insert([
+        "name"=> "Sharifi High School",
+        "whatsapp-link"=> "0795453222"
+       ]);
     }
 }
