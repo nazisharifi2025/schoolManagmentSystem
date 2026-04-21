7ceda4f56cda3944b7a51bdb246f686b65cfef95
nazi sharifi <banonazisharifi@gmail.com>
2026-04-21 23:49:38 +0430

    add new commit

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,8 +1,44 @@
-<x-app-layout>
-  <div class=" min-h-screen h-fit w-full grid grid-cols-5">
-    <div class=" col-span-1 h-full w-full bg-primary flex flex-col gap-2">
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    </div>
-    <div class=" col-span-4 h-full w-full "></div>
+        <title>Laravel</title>
+        @vite(['resources/js/slider.js'])
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+        <!-- Styles -->
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    </head>
+    <body class="antialiased font-sans  ">
+   <x-app-layout>
+  <div class=" min-h-screen h-fit w-full grid grid-cols-5">
+
+    <livewire:dashboard.sidebar />
+       <div class=" col-span-4 grid grid-cols-2 gap-2 h-full w-full ">
+         <div class = "p-4 bg-white/70 h-fit">
+            <h1>Employees statatistics</h1>
+         </div>
+         <div class = "p-4 bg-white/70 h-fit">
+            <h1> Salaries statisticss
+            </h1>
+         </div>
+         <div class = "p-4 bg-white/70 h-fit">
+            <h1> Contracts statisticss
+            </h1>
+         </div>
+         <div class = "p-4 bg-white/70 h-fit">
+            <h1> Payroll statisticss
+            </h1>
+         </div>
+         
+       </div>
   </div>
 </x-app-layout>
+{{-- <iframe src="https://www.youtube.com/embed/dQw4w9WgXcQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe> --}}
+    </body>
+</html>
+
---
+++ resources/views/livewire/dashboard/sidebar.blade.php
@@ -0,0 +1,29 @@
+<div class=" col-span-1 h-full w-full bg-primary flex flex-col gap-4">
+        <h1 class="text-3xl px-2 py-4 border-gray-800 border-b font-bold text-brand">Dashboard</h1>
+        <div class=" flex items-center px-3 gap-4">
+            <a href="#">
+               <i class="fas fa-file-signature text-xl text-white"></i>
+    <span class="text-white font-bold">contract</span>
+
+            </a>
+        </div>
+        <div class=" flex items-center px-3 gap-4">
+            <a href="#">
+            <i class="fas fa-credit-card text-xl text-white"></i>
+            <span class="text-white font-bold">payment</span>
+            </a>
+        </div>
+        <div class=" flex items-center px-3 gap-4">
+            <a href="#">
+            <i class="fas fa-money-check-alt text-xl text-white"></i>
+            <span class="text-white font-bold">payroll</span>
+            </a>
+        </div>
+        <div class=" flex items-center px-3 gap-4">
+            <a href="#">
+            <i class="fas fa-school text-xl text-white"></i>
+            <span class="text-white font-bold">school</span>
+            </a>
+        </div>
+    </div>
+ 
