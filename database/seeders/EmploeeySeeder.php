38a8d8c8411da4b1fb71b2c020983ccc7e016ab0
nazi sharifi <banonazisharifi@gmail.com>
2026-04-22 00:05:05 +0430

    add new seeder file

--- database/seeders/EmploeeySeeder.php
+++ database/seeders/EmploeeySeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class EmploeeySeeder extends Seeder
 {
@@ -12,6 +13,10 @@ class EmploeeySeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table('emploees')->insert([
+            [
+                "firstName"=> "Ali Ahmad"
+            ]
+        ]);
     }
 }
