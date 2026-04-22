551237d68b7bd68ca1d870c18943394d65d874f3
nazi sharifi <banonazisharifi@gmail.com>
2026-04-22 12:17:12 +0430

    add new seeder file

--- database/seeders/CantractSeeder.php
+++ database/seeders/CantractSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class CantractSeeder extends Seeder
 {
@@ -12,6 +13,19 @@ class CantractSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+           DB::table('Cantract')->insert([
+            [
+                'designation_id' => 1,
+                'employee_id' => 1,
+                'start_date' => '2024-01-01',
+                'end_date' => '2024-12-31',
+            ],
+            [
+                'designation_id' => 2,
+                'employee_id' => 2,
+                'start_date' => '2024-02-01',
+                'end_date' => '2025-02-01',
+            ],
+        ]);
     }
 }
