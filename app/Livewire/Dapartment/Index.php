3c4917c0048a7576b0eabb10db97def11c8d48bb
nazi sharifi <banonazisharifi@gmail.com>
2026-04-22 22:52:28 +0430

    add logeg livrwire file

--- app/Livewire/Dapartment/Index.php
+++ app/Livewire/Dapartment/Index.php
@@ -2,13 +2,14 @@
 
 namespace App\Livewire\Dapartment;
 
+use App\Models\departmint;
 use Livewire\Component;
 
 class Index extends Component
 {
     public $departmint ;
     public function mount(){
-        
+        $this->departmint = departmint::all();
     }
     public function render()
     {
