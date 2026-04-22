932ff6a9b99d352dfa6511c1896fda881a5fae1b
nazi sharifi <banonazisharifi@gmail.com>
2026-04-22 22:51:29 +0430

    add new seeder file

--- app/Livewire/Dapartment/Index.php
+++ app/Livewire/Dapartment/Index.php
@@ -6,6 +6,10 @@ use Livewire\Component;
 
 class Index extends Component
 {
+    public $departmint ;
+    public function mount(){
+        
+    }
     public function render()
     {
         return view('livewire.dapartment.index');
