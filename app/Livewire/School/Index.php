81896194c0a2f0cc2075e4bb09eb4c1f50c34c9f
nazi sharifi <banonazisharifi@gmail.com>
2026-04-22 22:44:44 +0430

    add logeg livrwire file

--- app/Livewire/School/Index.php
+++ app/Livewire/School/Index.php
@@ -6,6 +6,10 @@ use Livewire\Component;
 
 class Index extends Component
 {
+    $School = null;
+    public function mount(){
+        $this->School = Schools::all();
+    }
     public function render()
     {
         return view('livewire.school.index');
