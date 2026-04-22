9f21420cefb06950a237a7c0fa04f436c0a86b0f
nazi sharifi <banonazisharifi@gmail.com>
2026-04-22 12:38:49 +0430

    add new seeder file

--- database/seeders/EmploeeySeeder.php
+++ database/seeders/EmploeeySeeder.php
@@ -14,13 +14,21 @@ class EmploeeySeeder extends Seeder
     public function run(): void
     {
         DB::table('emploees')->insert([
-            [
+                       [
                 "firstName"=> "Ali Ahmad",
                 "lastName"=> "Ahmadi",
                 "tazkira"=> "091323",
                 "email"=> "AliAhmad@gmail.com",
                 "phone"=> "0798764532"
+            ],
+            [
+                "firstName"=> "Samiullah",
+                "lastName"=> "Karimi",
+                "tazkira"=> "091324",
+                "email"=> "sami@gmail.com",
+                "phone"=> "0781234567"
             ]
+
         ]);
     }
 }
--- database/seeders/paymentSeeder.php
+++ database/seeders/paymentSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class paymentSeeder extends Seeder
 {
@@ -12,6 +13,21 @@ class paymentSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+          DB::table('payments')->insert([
+            [
+                "payment_amount" => 15000,
+                "employee_id" => 1,
+                "payroll_id" => 1,
+                "created_at" => now(),
+                "updated_at" => now(),
+            ],
+            [
+                "payment_amount" => 20000,
+                "employee_id" => 2,
+                "payroll_id" => 2,
+                "created_at" => now(),
+                "updated_at" => now(),
+            ],
+        ]);
     }
 }
