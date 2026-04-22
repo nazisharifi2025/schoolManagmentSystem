80c320aa3405fd3c2f198dc0c4132844e6b39069
nazi sharifi <banonazisharifi@gmail.com>
2026-04-22 12:13:38 +0430

    add new seeder file

--- database/seeders/designationSeeder.php
+++ database/seeders/designationSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class designationSeeder extends Seeder
 {
@@ -12,6 +13,27 @@ class designationSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table("designation")->insert([
+             [
+                'name' => 'Manager',
+                'department_id' => 1,
+            ],
+            [
+                'name' => 'Assistant Manager',
+                'department_id' => 1,
+            ],
+            [
+                'name' => 'HR Officer',
+                'department_id' => 2,
+            ],
+            [
+                'name' => 'Accountant',
+                'department_id' => 3,
+            ],
+            [
+                'name' => 'Software Engineer',
+                'department_id' => 4,
+            ],
+        ]);
     }
 }
