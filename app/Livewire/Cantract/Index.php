a170b5e6424bddf501bbcf04cfbad5ffdf8cd388
nazi sharifi <banonazisharifi@gmail.com>
2026-04-22 22:55:25 +0430

    add logeg livrwire file

--- app/Livewire/Cantract/Index.php
+++ app/Livewire/Cantract/Index.php
@@ -2,11 +2,15 @@
 
 namespace App\Livewire\Cantract;
 
+use App\Models\Contract;
 use Livewire\Component;
 
 class Index extends Component
 {
     public $cantract ;
+    public function mount(){
+        $this->cantract  = Contract::all();
+    }
     public function render()
     {
         return view('livewire.cantract.index');
