3dc6f652328e7ec3519d487fcef00ca07b9348a8
nazi sharifi <banonazisharifi@gmail.com>
2026-04-23 23:50:42 +0430

    add logeg livrwire file

--- app/Livewire/Payroll/Index.php
+++ app/Livewire/Payroll/Index.php
@@ -6,6 +6,10 @@ use Livewire\Component;
 
 class Index extends Component
 {
+    public $payroll ;
+    public function mount(){
+        
+    }
     public function render()
     {
         return view('livewire.payroll.index');
