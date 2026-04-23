6f5435f85ce42dd99513560d9bbca0fe79ea7cd9
nazi sharifi <banonazisharifi@gmail.com>
2026-04-23 23:57:27 +0430

    add logeg livrwire file

--- app/Livewire/Designation/Index.php
+++ app/Livewire/Designation/Index.php
@@ -2,11 +2,15 @@
 
 namespace App\Livewire\Designation;
 
+use App\Models\Designation;
 use Livewire\Component;
 
 class Index extends Component
 {
     public $designation;
+    public function mount(){
+        $this->designation = Designation::all();
+    }
     public function render()
     {
         return view('livewire.designation.index');
