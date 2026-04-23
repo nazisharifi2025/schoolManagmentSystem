0441c101ce569bbf368696193af63f867eb007c4
nazi sharifi <banonazisharifi@gmail.com>
2026-04-23 23:59:39 +0430

    add logeg livrwire file

--- app/Livewire/School/Add.php
+++ app/Livewire/School/Add.php
@@ -2,10 +2,15 @@
 
 namespace App\Livewire\School;
 
+use App\Models\School;
 use Livewire\Component;
 
 class Add extends Component
 {
+    public $school;
+    public function mount(){
+        $this->school = new School();
+    }
     public function render()
     {
         return view('livewire.school.add');
