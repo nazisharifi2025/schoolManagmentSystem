cf0207022baf1d67931cfba87d907108ab1ef8ec
nazi sharifi <banonazisharifi@gmail.com>
2026-04-24 23:23:46 +0430

    add update function

--- app/Livewire/Payroll/Add.php
+++ app/Livewire/Payroll/Add.php
@@ -6,6 +6,13 @@ use Livewire\Component;
 
 class Add extends Component
 {
+    public $payroll;
+    public function mount(){
+        $this->payroll = new payroll();
+    }
+    public function create(Request $request){
+
+    }
     public function render()
     {
         return view('livewire.payroll.add');
