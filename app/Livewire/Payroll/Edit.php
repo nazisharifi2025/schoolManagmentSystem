1efac3e855722523bfea964ee9980e4eafb0a9a8
nazi sharifi <banonazisharifi@gmail.com>
2026-04-24 23:31:14 +0430

    add update function

--- app/Livewire/Payroll/Edit.php
+++ app/Livewire/Payroll/Edit.php
@@ -6,6 +6,11 @@ use Livewire\Component;
 
 class Edit extends Component
 {
+    public $payroll;
+    public $id ;
+    public function update(Request $request , string $id){
+        $this->id = $id;
+    }
     public function render()
     {
         return view('livewire.payroll.edit');
