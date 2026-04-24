fa274c793b5fb4402c4e2b48af50440c9374935e
nazi sharifi <banonazisharifi@gmail.com>
2026-04-24 23:13:14 +0430

    add update function

--- app/Livewire/School/Edit.php
+++ app/Livewire/School/Edit.php
@@ -6,6 +6,11 @@ use Livewire\Component;
 
 class Edit extends Component
 {
+    public $id ;
+    public $schoolInfo ;
+    public function update(Request $request , string $id){
+        $this->id = $id ;
+    }
     public function render()
     {
         return view('livewire.school.edit');
