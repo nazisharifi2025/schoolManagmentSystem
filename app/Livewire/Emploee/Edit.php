06b436b58b7551f868417ec28ec7d677d948a229
nazi sharifi <banonazisharifi@gmail.com>
2026-04-25 09:06:06 +0430

    add work in add file

--- app/Livewire/Emploee/Edit.php
+++ app/Livewire/Emploee/Edit.php
@@ -2,10 +2,20 @@
 
 namespace App\Livewire\Emploee;
 
+use App\Models\Emploee;
+use Illuminate\Http\Request;
 use Livewire\Component;
 
 class Edit extends Component
 {
+    public $id ;
+    public $emploeeData;
+    public function update(Request $request , string $id){
+        $this->id = $id ;
+    }
+    public function mount(){
+        $this->emploeeData = Emploee::findOrFail($this->id);
+    }
     public function render()
     {
         return view('livewire.emploee.edit');
