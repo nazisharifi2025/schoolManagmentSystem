5cb84facc61387a65a4ad11409911977c82f4c2e
nazi sharifi <banonazisharifi@gmail.com>
2026-04-25 09:18:47 +0430

    add work in add file

--- app/Livewire/Dapartment/Edit.php
+++ app/Livewire/Dapartment/Edit.php
@@ -2,10 +2,20 @@
 
 namespace App\Livewire\Dapartment;
 
+use App\Models\departmint;
+use Illuminate\Http\Request;
 use Livewire\Component;
 
 class Edit extends Component
 {
+    public $id ;
+    public $departmentData;
+    public function update(Request $request,string $id){
+        $this->id = $id;
+    }
+    public function mount(){
+        $this->departmentData = departmint::findOrFail($this->id);
+    }
     public function render()
     {
         return view('livewire.dapartment.edit');
