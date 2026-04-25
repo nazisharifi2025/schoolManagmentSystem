50c007b30302001046a1ca4d1d2481f1ae1c704c
nazi sharifi <banonazisharifi@gmail.com>
2026-04-25 09:10:07 +0430

    add work in add file

--- app/Livewire/Designation/Add.php
+++ app/Livewire/Designation/Add.php
@@ -2,10 +2,26 @@
 
 namespace App\Livewire\Designation;
 
+use App\Models\Designation;
+use Illuminate\Http\Request;
 use Livewire\Component;
 
 class Add extends Component
 {
+    public $Designation ;
+    public function mount(){
+        $this->Designation = new Designation();
+    }
+    public function create(Request $request){
+        $request->validate([
+             "name"=> "required|string|min:3",
+        "department_id"=> "required|integer"
+        ]);
+        $this->Designation->create([
+             "name"=> $request->name,
+        "department_id"=> $request->department_id
+        ]);
+    }
     public function render()
     {
         return view('livewire.designation.add');
