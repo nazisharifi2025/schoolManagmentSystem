7fe89ba8cbac0a588c53d44f9371b7c20215f93d
nazi sharifi <banonazisharifi@gmail.com>
2026-04-25 08:48:08 +0430

    add work in add file

--- app/Livewire/Emploee/Add.php
+++ app/Livewire/Emploee/Add.php
@@ -3,6 +3,7 @@
 namespace App\Livewire\Emploee;
 
 use App\Models\Emploee;
+use Illuminate\Http\Request;
 use Livewire\Component;
 
 class Add extends Component
@@ -10,6 +11,16 @@ class Add extends Component
      public $emploee;
      public function mount(){
         $this->emploee = new Emploee();
+     }
+     public function Create(Request $request){
+        $request->validate([
+        "firstName" => "required|string|min:4",
+        "LastName"=> "required|string|min:6",
+        "tazkera"=> "required|integer",
+        "email" => "required|string",
+        "phone"=> "nullable|string|max:10",
+        "designation_id"=> "required|integer"
+        ]);
      }
     public function render()
     {
