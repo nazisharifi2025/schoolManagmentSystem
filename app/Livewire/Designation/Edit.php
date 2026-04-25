40e0cc7f29c00d9e969879761951723056ee10d2
nazi sharifi <banonazisharifi@gmail.com>
2026-04-25 09:13:20 +0430

    add work in add file

--- app/Livewire/Designation/Edit.php
+++ app/Livewire/Designation/Edit.php
@@ -2,10 +2,20 @@
 
 namespace App\Livewire\Designation;
 
+use App\Models\Designation;
+use Illuminate\Http\Request;
 use Livewire\Component;
 
 class Edit extends Component
 {
+    public $id ;
+    public $designationData;
+    public function update(Request $request , string $id){
+        $this->id = $id ;
+    } 
+    public function mount(){
+        $this->designationData = Designation::findOrFail($id);
+    }
     public function render()
     {
         return view('livewire.designation.edit');
